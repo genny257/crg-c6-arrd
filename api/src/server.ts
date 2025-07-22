4c71bd21706e472e81e727d287dd5a5cde8683ca
genny bibang <gennybibang@gmail.com>
2025-07-22 04:21:49 +0000

    change mecenat par Mécénat d'Entreprise au niveau du lien.  cré des fonc

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
 import missionRoutes from './routes/mission.routes';
 import blogRoutes from './routes/blog.routes';
 import eventRoutes from './routes/event.routes';
@@ -14,7 +17,6 @@ import userRoutes from './routes/user.routes';
 // Charger les variables d'environnement
 dotenv.config();
 
-
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -23,7 +25,7 @@ const swaggerOptions = {
   swaggerDefinition: {
     openapi: '3.0.0',
     info: {
-      title: 'API de gestion de missions',
+      title: 'API de gestion de la Croix-Rouge Gabonaise',
       version: '1.0.0',
       description: 'Documentation de l\'API pour la gestion des missions, des volontaires, etc.',
     },
@@ -34,26 +36,38 @@ const swaggerOptions = {
       },
     ],
   },
-  // Chemin vers les fichiers contenant les annotations Swagger
   apis: ['./src/routes/*.ts'],
 };
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
-// --- Fin de la configuration de Swagger ---
 
+// --- Middlewares de Sécurité ---
+// Ajoute des en-têtes de sécurité HTTP importants
+app.use(helmet());
 
-// --- Middlewares ---
+// Limite le nombre de requêtes par IP pour prévenir les attaques par force brute
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limite chaque IP à 100 requêtes par fenêtre de 15 minutes
+	standardHeaders: true,
+	legacyHeaders: false, 
+    message: 'Trop de requêtes depuis cette adresse IP, veuillez réessayer après 15 minutes.',
+});
+app.use(limiter);
+
+// --- Middlewares Généraux ---
 // Activer CORS pour autoriser les requêtes cross-origin
 app.use(cors());
-// Parser les corps de requête JSON
-app.use(express.json());
+
+// Parser les corps de requête JSON (avec une limite de taille pour la sécurité)
+app.use(express.json({ limit: '10kb' }));
 
 
 // --- Routes ---
 // Route pour la documentation de l'API
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// Routes de l'API pour les missions
+// Routes de l'API
 app.use('/api', missionRoutes);
 app.use('/api', blogRoutes);
 app.use('/api', eventRoutes);
