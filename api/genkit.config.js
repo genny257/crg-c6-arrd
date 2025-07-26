17c98c9ec5f9003795a21d672861426c1b659d47
genny bibang <gennybibang@gmail.com>
2025-07-26 03:54:01 +0000

    [ERROR] 03:53:12 Error: Cannot find module '@genkit-ai/dotprompt'

--- api/genkit.config.js
+++ api/genkit.config.js
@@ -1,11 +1,10 @@
 // api/genkit.config.js
 import { googleAI } from '@genkit-ai/googleai';
 import { genkit } from 'genkit';
-import { dotprompt } from '@genkit-ai/dotprompt';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env' });
 
 export const ai = genkit({
-  plugins: [dotprompt(), googleAI({ apiKey: process.env.GEMINI_API_KEY })],
+  plugins: [googleAI({ apiKey: process.env.GEMINI_API_KEY })],
 });
