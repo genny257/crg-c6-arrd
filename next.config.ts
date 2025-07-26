0ed1b5b22689c0f90375edf91dbd1931a399ab07
genny bibang <gennybibang@gmail.com>
2025-07-26 05:43:01 +0000

    continus

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,39 @@
+
 import type {NextConfig} from 'next';
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  child-src 'self';
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'self';
+  connect-src *;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+   {
+    key: 'Permissions-Policy',
+    value: "camera=(), microphone=(), geolocation=(), payment=()"
+  }
+];
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -7,6 +42,15 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
   images: {
     remotePatterns: [
@@ -19,6 +63,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestudio.ai',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.weserv.nl',
         port: '',
         pathname: '/**',
       },
