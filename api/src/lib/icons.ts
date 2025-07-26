741c64149eaa35b7fcbb4a57f8aa292358f2cb3e
genny bibang <gennybibang@gmail.com>
2025-07-26 09:49:48 +0000

    ==> Cloning from https://github.com/genny257/crg-c6-arrd ==> Checking ou

---
+++ api/src/lib/icons.ts
@@ -0,0 +1,18 @@
+
+import { Network, Users, Archive, Truck, Banknote, HeartPulse, HandHeart, Shield, GraduationCap, ClipboardCheck, Siren, Soup } from "lucide-react";
+import type { LucideIcon } from "lucide-react";
+
+export const allPoolIcons: { [key: string]: LucideIcon } = {
+  HeartPulse,
+  Users,
+  ClipboardCheck,
+  Siren,
+  HandHeart,
+  Soup,
+  Archive,
+  Banknote,
+  Truck,
+  Shield,
+  GraduationCap,
+  Network
+};
